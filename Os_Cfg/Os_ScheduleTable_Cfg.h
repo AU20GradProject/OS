3a6d0f87f500d1853b4ecd32ca51425b11a20078
Bakr <mohamedbakr9818@gmaill.com>
2020-03-02 08:50:33 +0200

    update cnfgs

--- Os_Cfg/Os_ScheduleTable_Cfg.h
+++ Os_Cfg/Os_ScheduleTable_Cfg.h
@@ -54,21 +54,25 @@
         {\
             .OsScheduleTblExpPointOffset = 1000,\
             .PointTasks = {1,0},\
+            .PointEvents = {0,0,0},\
             .OsScheduleTblAdjustableExpPoint = 0,\
         },\
         {\
             .OsScheduleTblExpPointOffset = 1000,\
             .PointTasks = {1,7},\
+            .PointEvents = {0,0,0},\
             .OsScheduleTblAdjustableExpPoint = 0,\
         },\
         {\
             .OsScheduleTblExpPointOffset = 1000,\
             .PointTasks = {1,2},\
+            .PointEvents = {0,0,0},\
             .OsScheduleTblAdjustableExpPoint = 0,\
         },\
         {\
             .OsScheduleTblExpPointOffset = 1000,\
             .PointTasks = {1,9},\
+            .PointEvents = {0,0,0},\
             .OsScheduleTblAdjustableExpPoint = 0,\
         },\
 }
